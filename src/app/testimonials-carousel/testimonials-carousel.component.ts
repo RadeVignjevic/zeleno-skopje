4ba7fb07ecbced851b4b39e447de2a568201d476
Davor Barishic-Jaman <cro.davor@outlook.com>
2018-12-18 17:35:44 +0100

    fix autocomplete city in form

--- src/app/testimonials-carousel/testimonials-carousel.component.ts
+++ src/app/testimonials-carousel/testimonials-carousel.component.ts
@@ -7,37 +7,29 @@ import { NguCarousel } from '@ngu/carousel';
   styleUrls: ['./testimonials-carousel.component.scss']
 })
 export class TestimonialsCarouselComponent implements OnInit {
-  @Input('backgroundGray') public backgroundGray;
+  @Input('backgroundGray') public backgroundGray: any;
   public carouselOptions: NguCarousel;
   public testimonials = [{
-    logo: 'assets/images/mock-logo-4.png',
-    photo: 'assets/images/face-1.jpg',
     text: `“I’ve tried using different softwares. The computer is not my strong side. 
     There is excellent support behind DevEgret and people to walk you through it. 
     If you have any questions they’ll go over that and explain to you how to do that. ”`,
     title: 'Jhone Doe',
-    subtitle: 'Product Manager'
+    city: 'Skopje'
   }, {
-    logo: 'assets/images/mock-logo-2.png',
-    photo: 'assets/images/face-2.jpg',
     text: `"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi voluptas vero iusto fuga quos totam eius,
     atis magnam tempora doloribus ducimus dolorem culpa animi beatae tenetur! Sapiente, quia tempora."`,
     title: 'Adam Smith',
-    subtitle: 'CEO'
+    city: 'Kavadarci'
   }, {
-    logo: 'assets/images/mock-logo-3.png',
-    photo: 'assets/images/face-3.jpg',
     text: `"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi voluptas vero iusto fuga quos totam eius,
     atis magnam tempora doloribus ducimus dolorem culpa animi beatae tenetur! Sapiente, quia tempora."`,
     title: 'Jhone White',
-    subtitle: 'Software Engineer'
+    city: 'Skopje'
   }, {
-    logo: 'assets/images/mock-logo-1.png',
-    photo: 'assets/images/face-4.jpg',
     text: `"Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit modi voluptas vero iusto fuga quos totam eius,
     atis magnam tempora doloribus ducimus dolorem culpa animi beatae tenetur! Sapiente, quia tempora."`,
     title: 'Jessica Hiche',
-    subtitle: 'CEO'
+    city: 'Bitola'
   }]
   constructor() { }
 
