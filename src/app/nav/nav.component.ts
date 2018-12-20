92720b1e8baca4d367f138d99e91f6de3b7a12f8
Davor Barishic-Jaman <cro.davor@outlook.com>
2018-12-20 15:20:40 +0100

    videos

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -4,6 +4,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { WINDOW } from "../window.service";
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 
 @Component({
@@ -23,7 +24,8 @@ export class NavComponent {
 
   constructor(private breakpointObserver: BreakpointObserver,
     @Inject(DOCUMENT) private document: Document,
-    @Inject(WINDOW) private window: Window) { }
+    @Inject(WINDOW) private window: Window,
+    private overlay: OverlayContainer) { }
 
   @HostListener("window:scroll", [])
   onWindowScroll() {
@@ -38,6 +40,29 @@ export class NavComponent {
   scroll(id) {
     let el = document.getElementById(id);
     el.scrollIntoView( {behavior: 'smooth', block: 'start'});
+  }
+
+  toggleTheme(): void {
+    console.log("TOGGLE");
+    console.log(this.overlay.getContainerElement().classList);
+    if (this.overlay.getContainerElement().classList.contains("dark-theme")) {
+      this.overlay.getContainerElement().classList.remove("light-theme");
+      this.overlay.getContainerElement().classList.add("dark-theme");
+    } else if (this.overlay.getContainerElement().classList.contains("dark-theme")) {
+      this.overlay.getContainerElement().classList.remove("dark-theme");
+      this.overlay.getContainerElement().classList.add("light-theme");
+    } else {
+      this.overlay.getContainerElement().classList.add("dark-theme");
+    }
+    if (document.body.classList.contains("light-theme")) {
+      document.body.classList.remove("light-theme");
+      document.body.classList.add("dark-theme");
+    } else if (document.body.classList.contains("dark-theme")) {
+      document.body.classList.remove("dark-theme");
+      document.body.classList.add("light-theme");
+    } else {
+      this.overlay.getContainerElement().classList.add("dark-theme");
+    }
   }
 }
 
