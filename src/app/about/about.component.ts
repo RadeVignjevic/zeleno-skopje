472b834cc14bb66a1d9c0859bbed91e6502d7c71
Davor Barishic-Jaman <cro.davor@outlook.com>
2018-12-20 01:29:40 +0100

    add about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,26 +3,21 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  faFacebook = faFacebookF;
+  faLinkedin = faLinkedinIn;
+  faTwitter = faTwitter;
 
   constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-    this.matIconRegistry.addSvgIcon(
-      `facebook-icon`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/img/fbb.svg`)
-    );
-    this.matIconRegistry.addSvgIcon(
-      `linkdin-icon`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/img/linkdin.svg`)
-    );
-    this.matIconRegistry.addSvgIcon(
-      `twitter-icon`,
-      this.domSanitizer.bypassSecurityTrustResourceUrl(`../../assets/img/twitter.svg`)
-    );
   }
 
   ngOnInit(): void {
